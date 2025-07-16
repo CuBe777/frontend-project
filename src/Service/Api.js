85f4990105e7fcaf1e05fa93d08b091895d0e06d
CuBe777 <70326468+CuBe777@users.noreply.github.com>
2025-07-16 11:58:02 +0530

    Update Api.js

--- src/Service/Api.js
+++ src/Service/Api.js
@@ -4,7 +4,6 @@ export const API = axios.create({baseURL: "http://localhost:8080"});
 export const addEmp = (data) => API.post("/employee/create", data);
 export const addDep = (data) => API.post("/department/create", data);
 export const moveEmp = (id, newDeptId) => API.put(`/employee/move/${id}?deptId=${newDeptId}`);
-//export const changeHead = (id, data) => API.post(`/deparment/head/${id}`, data);
 export const employeeList = () => API.get("/employee/getall");
 export const departmentList = () => API.get("/department/getall");
 export const employeeListInDep = (id) => API.get(`/employee/getByDepartment/${id}`)
